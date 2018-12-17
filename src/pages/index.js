8f87dd5f93fc4e68c2c81e8b56d10e9adbb0601c
Gaya Kessler <gaya.kessler@gmail.com>
2018-12-17 14:36:52 +0100

    start page updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ const IndexPage = () => (
                 what Thyme is good at.
               </p>
               <p>
-                Log how much time you spent on a task, perfect for logging hours
+                Log how much time you spent on a task. Perfect for logging hours
                 and billing clients.
               </p>
             </Grid.Column>
@@ -45,8 +45,8 @@ const IndexPage = () => (
                 Privacy First
               </Header>
               <p>
-                Thyme values your privacy. Every feature is developed with privacy
-                at the top of the list.
+                Thyme values your privacy. Every feature is developed with a
+                privacy-first security approach.
               </p>
               <p>
                 All the data you enter is yours, and yours alone by default.
@@ -58,8 +58,8 @@ const IndexPage = () => (
                 Reporting
               </Header>
               <p>
-                Create and export reports of your entered hours for your archive
-                and getting insights in the hours for spent during the week.
+                Create and export reports of your timesheets for your archive
+                and get insights in the hours you spent during the week.
               </p>
               <p>
                 Reports can be saved as PDF files for backups or as quick reports
@@ -75,9 +75,9 @@ const IndexPage = () => (
             Privacy Centric Time Tracking
           </Header>
           <p>
-            You don't need other people (and companies) looking over your shoulder
-            when keeping your time. With Thyme you control and keep your
-            own data right in your browser.
+            You don't need other people (and companies) looking over your
+            shoulder when keeping your time. With Thyme you control and keep
+            your own data right in your browser.
           </p>
         </Container>
 
@@ -102,8 +102,8 @@ const IndexPage = () => (
                 can create a report with the time you already logged.
               </p>
               <p>
-                This includes a more detailed version with your notes. Reports can
-                be easily exported to PDF or printed.
+                This includes a more detailed version with your notes and more.
+                Also, reports can be easily exported to PDF or printed.
               </p>
             </Grid.Column>
             <Grid.Column floated="right" width={8}>
