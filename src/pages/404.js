14b9f9ba63e936cd1ed57da62ce4dec7b5335290
Gaya Kessler <gaya.kessler@gmail.com>
2018-12-17 14:38:11 +0100

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = () => (
     <Segment style={{ padding: '4em 0' }} vertical>
       <Container>
         <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>You just hit a route that doesn&#39;t exist... Whoops!</p>
       </Container>
     </Segment>
   </Layout>
